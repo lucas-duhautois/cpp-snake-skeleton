b1a580f105f7d3012e809cdc965ddbb3338227bf
Lucas <luc.duhautois@gmail.com>
2024-12-12 13:33:55 +0100

    snake class

--- snake.cpp
+++ snake.cpp
@@ -9,153 +9,259 @@
 #include "time.h"
 #include "stdlib.h"
 
-#include "mes_fonctions.hpp"
+class board {
+  public :
+  const int nx;
+  const int ny;
+  std::vector<int> bg;
 
-// 😇 Je suis gentil je vous ai fait le tracé du terrain 😇
-std::vector<int> backgroundSetup(const int &nx, const int &ny)
-{
-  std::vector<int> bg(nx * ny, 0);
-  int i;
-  for (i = 0; i < nx; i++)
+  board (const int nx, const int ny) : nx{nx}, ny{ny}, bg(nx*ny, 0)
   {
-    bg[i] = 1;
-  }
-  for (i = 0; i < nx; i++)
-  {
-    bg[(ny - 1) * nx + i] = 1;
-  }
-  for (i = 0; i < ny; i++)
-  {
-    bg[i * nx] = 1;
-  }
-  for (i = 0; i < ny; i++)
-  {
-    bg[i * nx + nx - 1] = 1;
   }
 
-  return bg;
-}
+  ~board() = default;
+
+  void setup () {
+    int i;
+    for (i = 0; i < nx; i++){
+      bg[i] = 1;
+    }
+    for (i = 0; i < nx; i++){
+      bg[(ny - 1) * nx + i] = 1;
+    }
+    for (i = 0; i < ny; i++){
+      bg[i * nx] = 1;
+    }
+    for (i = 0; i < ny; i++){
+      bg[i * nx + nx - 1] = 1;
+    }
+  }
+
+  void clear () {
+    int out = system("clear");
+    if (out != 0)
+    {
+      std::cerr << "clear command failed" << std::endl;
+      exit(1);
+    }
+  }
+
+  void print() {
+    for (int j = 0; j < ny; j++)
+    {
+      for (int i = 0; i < nx; i++)
+      {
+        if (bg[i + j * nx] == 1)
+        {
+          std::cout << "#";
+        }
+        else if (bg[i + j * nx] == 2)
+        {
+          std::cout << "@";
+        }
+        else if (bg[i + j * nx] == 3)
+        {
+          std::cout << "o";
+        }
+        else
+        {
+          std::cout << " ";
+        }
+      }
+      std::cout << std::endl;
+    }
+  }
+
+  void sleep (const int &ms) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(ms));
+  }
+};
+
+class snake {
+  public:
+  std::pair<int, int> head;
+  std::vector<std::pair<int, int>> body;
+
+  snake (const int snake_len) {
+    head = std::make_pair(10, 10);
+    for (int i = 1; i < snake_len; i++){
+      body.push_back(std::make_pair(10+i, 10));
+    }
+  }
+
+  ~snake () = default;
+
+  void add (board &bg) {
+    bg.bg[head.second * bg.nx + head.first] = 3;
+    for (auto &c : body) {
+      int index = c.second * bg.nx + c.first ;
+      bg.bg[index] = 3;
+    }
+  }
+
+  void remove (board &bg) {
+    bg.bg[head.second * bg.nx + head.first] = 0;
+    for (auto &c : body) {
+      int index = c.second * bg.nx + c.first ;
+      bg.bg[index] = 0;
+    }
+  }
+
+  bool verify () {
+    for (auto &c : body) {
+      if (head == c) {
+        return false;
+      } 
+    }
+    return true;
+  }
+
+  void move (char key, bool eat, const board &bg) {
+    int size = body.size();
+    if (eat){
+      body.push_back(body[size-1]);
+    }
+    for (int i = size-1; i > 0; i--) {
+      body[i] = body[i-1];
+    }
+    body[0] = head;
+
+    if (key == 'z') {
+      head.second--;
+    }
+    else if (key == 'q') {
+      head.first--;
+    }
+    else if (key == 's') {
+      head.second++;
+    }
+    else if (key == 'd') {
+      head.first++;
+    }
+    
+    if (head.first <= 0) {
+      head.first = bg.nx - 2;
+    }
+    else if (head.first >= bg.nx - 1) {
+      head.first = 1;
+    }
+
+    if (head.second <= 0) {
+      head.second = bg.ny - 2;
+    }
+    else if (head.second >= bg.ny - 1) {
+      head.second = 1;
+    }
+  }
+};
+
+class food {
+  public:
+  std::array<int, 2> apple;
+
+  food () : apple {0, 0}
+  {
+  }
+
+  ~food() = default;
+
+  void renew (const snake &player, const board &bg) {
+    bool test = true;
+    while (test) {
+      apple[0] = rand() % (bg.nx - 2) + 2;
+      apple[1] = rand() % (bg.ny - 2) + 2;
+      test = false;
+      for (auto &c : player.body) {
+        if (~test & ((c.first == apple[0]) & (c.second == apple[1]))) {
+          test = true;
+        }
+      }
+      if (~test & ((player.head.first == apple[0]) & (player.head.second == apple[1]))) {
+        test = true;
+      }
+    }
+  }
+
+  void print (board &bg) {
+    bg.bg[apple[1] * bg.nx + apple[0]] = 2;
+  }
+
+  bool eat (const snake &player) {
+    if ((apple[0] == std::get<0>(player.body[0])) && (apple[1] == std::get<1>(player.body[0]))){
+      return true;
+    }
+    if ((apple[0] == player.head.first) && (apple[1] == player.head.second)){
+      return true;
+    }
+    return false;
+  }
+};
 
 ///////////////////////////////////////////////////////////////////////////////
 /////// 🧑‍💻 FROM HERE you must complete 🧑‍💻 ////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-void add_snake(const std::vector<std::pair<int, int>> &snake, std::vector<int> &bg, int nx, int ny)
-{
-  for (auto &c : snake) {
-    int index = c.second * nx + c.first ;
-    bg[index] = 3;
-  }
-}
+static const int STDIN = 0;
+static bool initialized = false;
 
-void remove_snake(const std::vector<std::pair<int, int>> &snake, std::vector<int> &bg, int nx, int ny)
-{
-  for (auto &c : snake) {
-    int index = c.second * nx + c.first ;
-    bg[index] = 0;
-  }
-}
-
-std::array<int, 2> snake_movement(char key)
-{
-  std::array<int, 2> dxdy = {0, 0};
-  if (key == 'z') {
-    dxdy = {0, -1};
-  }
-  else if (key == 'q') {
-    dxdy = {-1, 0};
-  }
-  else if (key == 's') {
-    dxdy = {0, 1};
-  }
-  else if (key == 'd') {
-    dxdy = {1, 0};
-  }
-  return dxdy;
-}
-
-bool verifyBorder(std::vector<std::pair<int, int>> &snake, int nx, int ny)
-{
-  std::pair<int, int> head = snake[0];
-  for (int i = 1; i < snake.size()-1; i++) {
-    if (head == snake[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-std::vector<std::pair<int, int>> setupSnake(int snake_len)
-{
-  std::vector<std::pair<int, int>> snake(snake_len);
-  int i;
-  for (i = 0; i < snake_len; i++)
+int keyEvent()
   {
-    snake[i].first = 10 + i;
-    snake[i].second = 10;
+    if (!initialized)
+    {
+      termios term;
+      tcgetattr(STDIN, &term);
+      term.c_lflag &= ~ICANON;
+      tcsetattr(STDIN, TCSANOW, &term);
+      setbuf(stdin, NULL);
+      initialized = true;
+    }
+    int bytesWaiting;
+    // int bytesWaiting;
+    ioctl(STDIN, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
   }
-  return snake;
-}
 
-void update_snake_coordinates(std::vector<std::pair<int, int>> &snake, bool eat, std::array<int, 2> dxdy, int nx, int ny)
+void startGame(const int &snake_len, const int &nx, const int &ny, int &lap, const int &lapMin, const int &lapRate)
 {
-  int size = snake.size();
-  if (eat){
-    snake.push_back(snake[size-1]);
-  }
-  for (int i = size-1; i > 0; i--) {
-    snake[i] = snake[i-1];
-  }
-  snake[0].first += dxdy[0];
-  if (snake[0].first <= 0) {
-    snake[0].first = nx - 2;
-  }
-  else if (snake[0].first >= nx - 1) {
-    snake[0].first = 1;
-  }
-  snake[0].second += dxdy[1];
-  if (snake[0].second <= 0) {
-    snake[0].second = ny - 2;
-  }
-  else if (snake[0].second >= ny - 1) {
-    snake[0].second = 1;
-  }
-}
+  board background (nx, ny);
+  snake player (snake_len);
+  food yum;
 
-void startGame(int &lap, const int &nx, const int &ny, std::vector<std::pair<int, int>> &snake, std::vector<int> &bg, const int &lapMin, const int &lapRate)
-{
+  background.setup();
+  background.print();
+
   int timer = 0;
   int score = 0;
-  char key;
-  std::array<int, 2> dxdy = {1, 0};
-  std::array<int, 2> food = {0, 0};
-  internal::createFood(snake, bg, food, nx, ny);
+  char key = 'd';
+
+  yum.renew(player, background);
+  yum.print(background);
   while (true)
   {
-    internal::frameSleep(lap);
-    if (internal::keyEvent())
+    background.sleep(lap);
+    if (keyEvent())
     {
       std::cin >> key;
-      dxdy = snake_movement(key);
     }
-    internal::backgroundClear();
-    add_snake(snake, bg, nx, ny);
-    internal::printFrame(nx, ny, bg);
-    remove_snake(snake, bg, nx, ny);
-    bool out = verifyBorder(snake, nx, ny);
-    if (out == false)
+    background.clear();
+    player.add(background);
+    background.print();
+    player.remove(background);
+    bool out = player.verify();
+    if ((out == false) & (score > 0))
     {
       std::cerr << "💀💀💀 Game Over 💀💀💀" << std::endl;
       std::cerr << "Your score : " << score << std::endl;
       exit(1);
     }
-    bool eat = internal::eatFood(food, snake);
+    bool eat = yum.eat(player);
     if (eat)
     {
-      internal::createFood(snake, bg, food, nx, ny);
+      yum.renew(player, background);
+      yum.print(background);
       score++;
     }
-    update_snake_coordinates(snake, eat, dxdy, nx, ny);
+    player.move(key, eat, background);
     if (lap > lapMin) {
       if (timer == lapRate) {
       lap = lap - 1;
@@ -176,11 +282,6 @@ int main()
   const int lapMin = 50;
   const int lapRate = 5;
 
-  std::vector<int> background = backgroundSetup(nx, ny);
-  internal::printFrame(nx, ny, background);
-
-  std::vector<std::pair<int, int>> snake = setupSnake(snake_len);
-
-  startGame(lap, nx, ny, snake, background, lapMin, lapRate);
+  startGame(snake_len, nx, ny, lap, lapMin, lapRate);
   return 0;
 }
